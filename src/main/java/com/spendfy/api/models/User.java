05cd943fd957d0f210da2300275a99a314fb718f
LucasZaia <lucas.zaia30@gmail.com>
2024-04-24 09:00:49 -0300

    add spring security

--- src/main/java/com/spendfy/api/models/User.java
+++ src/main/java/com/spendfy/api/models/User.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.Valid;
+
 
 @Entity
 @Table(name = "users")
